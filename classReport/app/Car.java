866425af8d9ee2c8f1a976a90434c79972086686
cataniandrea <91258294+acataniUNIBO@users.noreply.github.com>
2022-04-28 12:13:16 +0200

    progetto

---
+++ classReport/app/Car.java
@@ -0,0 +1,32 @@
+class Car {
+    int wheels;
+    String model;
+    String tuMamma;
+
+    Car(int wheels, String model, String tuMamma) {
+        this.wheels = wheels;
+        this.model = model;
+        this.tuMamma = tuMamma;
+    }
+
+    public int getNumberOfWheels() {
+        return this.wheels;
+    }
+
+    public void setNumberOfWheels(int wheels) {
+        this.wheels = wheels;
+    }
+}
+
+class Truck extends Car {    
+    int cargos;
+
+    Truck(int cargos, int wheels, String model, String tuMamma) {
+        super(wheels, model, tuMamma);
+        this.cargos = cargos;
+    }
+}
+
+interface ApeCar { 
+    public boolean apeHasStereo();
+}
